3a78628387d2b21eb756a70c3cdccfee6b0eca72
Jeremy Schur <jeremy.schur@telosalliance.com>
2025-05-02 14:20:04 -0600

    Add db schema

--- init_db.sql
+++ init_db.sql
@@ -1,7 +1,6 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(100),
-  email VARCHAR(100)
+CREATE TABLE IF NOT EXISTS channel (
+    id VARCHAR(13) PRIMARY KEY, -- Channel_id with max length of 13 characters
+    avg_sentiment DECIMAL(3, 2), -- Average sentiment score between -1.00 and 1.00
+    last_read VARCHAR(18), -- Timestamp of last read message
+    name VARCHAR(80) -- Slack channels can only be 80 characters long
 );
-
-INSERT INTO users (username, email) VALUES ('john_doe', 'john@example.com');
