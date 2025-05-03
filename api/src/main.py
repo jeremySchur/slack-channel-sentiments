c8e60b68cbb5d4f21e8bcd8cb165699499c4a552
Jeremy Schur <jeremy.schur@telosalliance.com>
2025-05-03 17:38:01 -0600

    Add CORS middleware

--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import psycopg
 from pydantic import BaseModel
 from typing import List
@@ -19,11 +20,19 @@ class SentimentData(BaseModel):
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("FRONTEND_URL") or "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/getChannelData", response_model=List[SentimentData])
 async def get_channel_data():
     with psycopg.connect(**DB_PARAMS) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT name, avg_sentiment, last_read FROM channel")
+            cur.execute("SELECT name, avg_sentiment, last_read FROM channels")
             data = cur.fetchall()
 
     return [SentimentData(name=row[0], avg_sentiment=row[1], last_read=row[2]) for row in data]
